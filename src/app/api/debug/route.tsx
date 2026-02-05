d7fd6a1b967306081eff1edf1a3f9bb326a4b5f4
Karl J. Overå <k@rl.run>
2026-02-05 13:11:11 +0100

    chore: debug route

--- src/app/api/debug/route.tsx
+++ src/app/api/debug/route.tsx
@@ -1,13 +1,9 @@
 import { getToken } from '@navikt/oasis'
 import { headers } from 'next/headers'
-import { notFound } from 'next/navigation'
 
-import { bundledEnv } from '@lib/env'
 import { userInfo } from '@services/auth/auth'
 
 export async function GET(): Promise<Response> {
-    if (bundledEnv.runtimeEnv === 'prod-gcp') notFound()
-
     const headerStore = await headers()
     const token = getToken(headerStore)
     const user = await userInfo()
