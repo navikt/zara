e24e5e551cd282617928a70f2c7993d21204d494
Karl J. Overå <k@rl.run>
2026-02-05 14:50:07 +0100

    refactor: cleaner error handling in avatar proxy

--- src/app/api/avatar/[upn]/route.tsx
+++ src/app/api/avatar/[upn]/route.tsx
@@ -2,6 +2,7 @@ import { logger } from '@navikt/next-logger'
 import { requestAzureClientCredentialsToken } from '@navikt/oasis'
 
 import { bundledEnv } from '@lib/env'
+import { validateUserSession } from '@services/auth/auth'
 
 import { devAvatarBase64 } from '../../../../dev/dev-image'
 
@@ -11,45 +12,63 @@ import { devAvatarBase64 } from '../../../../dev/dev-image'
 export async function GET(_: Request, { params }: RouteContext<'/api/avatar/[upn]'>): Promise<Response> {
     const { upn } = await params
 
+    /**
+     * Only allow authenticated users to proxy avatars.
+     */
+    await validateUserSession()
+
+    /**
+     * Return a fixed avatar for dev user, 404 for fake "other" users.
+     */
     if (bundledEnv.runtimeEnv === 'local') {
-        if (upn === 'fake-dev-oid') {
-            return new Response(Buffer.from(devAvatarBase64, 'base64'), {
-                headers: { 'Content-Type': 'image/jpeg' },
-            })
-        } else {
-            return new Response(undefined, { status: 404 })
-        }
+        return handleLocalDevelopmentRequest(upn)
     }
 
     try {
-        const response = await requestAzureClientCredentialsToken('https://graph.microsoft.com/.default')
-        if (!response.ok) {
+        const clientCredentials = await requestAzureClientCredentialsToken('https://graph.microsoft.com/.default')
+        if (!clientCredentials.ok) {
             logger.error(
-                new Error(`Unable to acquire Azure client credentials token: ${response.error.message}`, {
-                    cause: response.error,
+                new Error(`Unable to acquire Azure client credentials token: ${clientCredentials.error.message}`, {
+                    cause: clientCredentials.error,
                 }),
             )
             return new Response(undefined, { status: 404 })
         }
 
-        return fetch(`https://graph.microsoft.com/v1.0/users/${upn}/photo/$value`, {
+        const avatarResponse = await fetch(`https://graph.microsoft.com/v1.0/users/${upn}/photo/$value`, {
             method: 'GET',
-            headers: { Authorization: `Bearer ${response.token}` },
-        }).then(async (res) => {
-            if (!res.ok) {
-                const contentType = res.headers.get('Content-Type')
-                logger.warn(
-                    new Error(`Unable to fetch user photo from MS Graph: ${res.statusText}`, {
-                        cause: contentType != null ? new Error(await res.text()) : undefined,
-                    }),
-                )
-                return new Response(undefined, { status: 404 })
-            }
-
-            return res
+            headers: { Authorization: `Bearer ${clientCredentials.token}` },
         })
-    } catch (e) {
-        logger.error(e)
+
+        /**
+         * Mask any MS Graph API errors as 404
+         */
+        if (!avatarResponse.ok) {
+            const contentType = avatarResponse.headers.get('Content-Type')
+            logger.warn(
+                new Error(`Unable to fetch user photo from MS Graph: ${avatarResponse.statusText}`, {
+                    cause: contentType != null ? new Error(await avatarResponse.text()) : undefined,
+                }),
+            )
+            return new Response(undefined, { status: 404 })
+        }
+
+        /**
+         * We got avatar, we can return it to the browser directly without touching the body.
+         */
+        return avatarResponse
+    } catch (error) {
+        logger.error(new Error('Unknown error while trying to fetch token from MS Graph API', { cause: error }))
+        return new Response(undefined, { status: 404 })
+    }
+}
+
+function handleLocalDevelopmentRequest(upn: string): Response {
+    if (upn === 'fake-dev-oid') {
+        return new Response(Buffer.from(devAvatarBase64, 'base64'), {
+            headers: { 'Content-Type': 'image/jpeg' },
+        })
+    } else {
         return new Response(undefined, { status: 404 })
     }
 }
