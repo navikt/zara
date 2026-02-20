9e42fe2ea572977672215235dc209944cf7f1fd8
Karl J. Overå <k@rl.run>
2026-02-20 20:19:28 +0100

    fix: only run migrations once

--- src/app/api/internal/is_ready/route.ts
+++ src/app/api/internal/is_ready/route.ts
@@ -1,15 +1,24 @@
 import { NextResponse } from 'next/server'
 import { logger } from '@navikt/next-logger'
+import { nextleton } from 'nextleton'
 
 import { getServerEnv } from '@lib/env'
 import { setupDailyOfficeCron, setupDailySummaryCron, setupWeeklyOfficeCron } from '@services/cron/setup-cron'
 import { runMigrations } from '@services/db/postgres/migrations'
 
+const migrationStatus = nextleton('migration-status', () => ({
+    executed: false,
+}))
+
 export async function GET(): Promise<NextResponse> {
     try {
         getServerEnv()
 
-        await runMigrations()
+        if (!migrationStatus.executed) {
+            await runMigrations().then(() => {
+                migrationStatus.executed = true
+            })
+        }
 
         setupDailySummaryCron()
         setupDailyOfficeCron()
