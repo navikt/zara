e75aa288ead020b9280bdb283cece774aaa1b255
Karl J. Overå <k@rl.run>
2026-05-18 14:22:29 +0200

    chore: better order for vakt-features

--- src/features/vakt/produksjons-feature-list.ts
+++ src/features/vakt/produksjons-feature-list.ts
@@ -1,18 +1,18 @@
 import { HeadHeartIcon, PillRectangleIcon } from '@navikt/aksel-icons'
 
 export const produksjonsFeatures = {
-    poisonPill: {
-        Icon: PillRectangleIcon,
-        title: 'Poison Pill Sykmelding',
-        href: '/vakt/poison-pill',
-        description: "Marker en sykmelding som 'poison pill', som betyr at den ikke skal prosesseres.",
-        auditlog: false,
-    },
     behandlerLookup: {
         Icon: HeadHeartIcon,
         title: 'Slå opp på HPR-nummer',
         href: '/vakt/hpr-lookup',
         description: 'Søk på HPR-nummer og se informasjon og helsepersonell i HPR-registeret og PDL.',
+        auditlog: false,
+    },
+    poisonPill: {
+        Icon: PillRectangleIcon,
+        title: 'Poison Pill Sykmelding',
+        href: '/vakt/poison-pill',
+        description: "Marker en sykmelding som 'poison pill', som betyr at den ikke skal prosesseres.",
         auditlog: false,
     },
 } as const
